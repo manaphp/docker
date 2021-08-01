38c48d694d6e3c0c98d469e35a0071a7bcd8591b
malongshu <182452649@qq.com>
2021-08-01 16:13:38 +0800

    增加latest标签

--- php/build.sh
+++ php/build.sh
@@ -10,12 +10,13 @@ set -xe
 PHP_VERSION=${1:-'php74'}
 IMAGE_TAG=${2:-$(date +%y%m%d)}
 IMAGE_URI="manaphp/${PHP_VERSION}:${IMAGE_TAG}"
+IMAGE_URI_LATEST="manaphp/${PHP_VERSION}:latest"
 
 BUILD_DIR=$(realpath $(dirname "$0"))
 
 if [ "$http_proxy" == '' ]; then
-  docker build --tag $IMAGE_URI --file $BUILD_DIR/${PHP_VERSION}.dockerfile $BUILD_DIR
+  docker build --tag $IMAGE_URI --tag $IMAGE_URI_LATEST --file $BUILD_DIR/${PHP_VERSION}.dockerfile $BUILD_DIR
 else
-  docker build --tag $IMAGE_URI --file $BUILD_DIR/${PHP_VERSION}.dockerfile --build-arg http_proxy=$http_proxy $BUILD_DIR
+  docker build --tag $IMAGE_URI --tag $IMAGE_URI_LATEST --file $BUILD_DIR/${PHP_VERSION}.dockerfile --build-arg http_proxy=$http_proxy $BUILD_DIR
 fi
-docker push $IMAGE_URI
+docker push $IMAGE_URI && docker push $IMAGE_URI_LATEST
